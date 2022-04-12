3326078b4ffe44d1445982ede047360133cf94ce
siddhant101 <59133332+siddhant101@users.noreply.github.com>
2022-04-12 07:22:32 +0530

    Create _nav.php

---
+++ _nav.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <div class="container-fluid">
+
+
+    <a class="navbar-brand" href="#" style="color:white">        
+    <strong style="color: red" >STOCK</strong>IMAGES
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        
+        <li class="nav-item">
+          <a class="nav-link" href="/loginsystem/login.php" style="color:white" >login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/loginsystem/signup.php" style="color:white">signup</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/loginsystem/welcome.php" style="color:white">Welcome</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="/loginsystem/my img.php" style="color:white">Images</a>
+       
+        </li>
+
+        <li class="nav-item">
+        <a class="nav-link" href="/loginsystem/my img.php" style="color:white">Images</a>
+       
+        </li>
+
+        
+    </div>
+  </div>
+</nav>
